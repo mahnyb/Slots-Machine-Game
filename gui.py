70e9489d9f7866362fd9146e00061369512e4c24
mahnyb <mahnybarazandehtar1998@gmail.com>
2024-04-27 19:16:08 +0300

    first

---
+++ gui.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk() # an instance of a window
+
+window.geometry("500x500")
+window.title("mahny's coding adventures")
+
+##icon = PhotoImage(file='pic.jpeg')
+## window.iconphoto(True, icon)
+window.config(background="pink")
+window.mainloop() # displays the window and listens for events 
